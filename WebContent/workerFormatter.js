75a121fdf8916b3632fdae59fc93d5ae0cf33f88
J. Brandt Buckley <brandt@runlevel1.com>
2016-01-30 14:44:18 -0700

    Improve HTML encoder

--- WebContent/workerFormatter.js
+++ WebContent/workerFormatter.js
@@ -4,8 +4,23 @@
  * Original author: Benjamin Hollis
  */
 
-function htmlEncode(t) {
-	return t != null ? t.toString().replace(/&/g, "&amp;").replace(/"/g, "&quot;").replace(/</g, "&lt;").replace(/>/g, "&gt;") : '';
+function htmlEncode(value) {
+	if (value === null) {
+		return '';
+	} else {
+		return value.toString()
+			.replace(/&/g, '&amp;')
+			.replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, function(value) {
+				var hi = value.charCodeAt(0);
+				var low = value.charCodeAt(1);
+				return '&#' + (((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000) + ';';
+			})
+			.replace(/([^\#-~| |!])/g, function(value) {
+				return '&#' + value.charCodeAt(0) + ';';
+			})
+			.replace(/</g, '&lt;')
+			.replace(/>/g, '&gt;');
+	}
 }
 
 function decorateWithSpan(value, className) {
@@ -16,7 +31,7 @@ function valueToHTML(value) {
 	var valueType = typeof value;
 	var output = "";
 
-	if (value == null) {
+	if (value === null) {
 		output += decorateWithSpan("null", "type-null");
 	} else if (value && value.constructor == Array) {
 		output += arrayToHTML(value);
